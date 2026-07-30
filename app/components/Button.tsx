111a8c467f8cca78e9e254995f9b7e5489d4ee0e
halimov <halimov@infomatika.ru>
2026-07-30 22:56:05 +0300

    redesign button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,21 +1,46 @@
+import { useState } from "react";
 import clsx from "clsx";
 import "@styles/button.css";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function Button({
     text,
+    onClick,
     isPrimary = true,
-    onClick
+    isDisabled,
 }: {
     text: string;
-    isPrimary?: boolean;
     onClick: () => Promise<void> | void;
+    isPrimary?: boolean;
+    isDisabled?: boolean;
 }) {
+
+    const [isPending, setIsPending] = useState(false);
+
+    async function handleClick() {
+        setIsPending(true);
+        try {
+            await onClick();
+        } finally {
+            setIsPending(false);
+        }
+    } 
+
     const buttonClassName = clsx("button", {
         "button_primary": isPrimary,
+        "button_pending": isPending,
+        "button_disbled": isDisabled,
     })
     return (
-        <button type="button" onClick={onClick} className={buttonClassName}>
-            {text}
+        <button type="button" onClick={handleClick} className={buttonClassName}>
+            <span className="button__text">
+                { isPending &&
+                    <span className="button__pending-icon">
+                        <AiOutlineLoading3Quarters />
+                    </span>
+                }
+                {text}
+            </span>
         </button>
     )
 }
