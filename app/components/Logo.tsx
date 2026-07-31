eb810a059caf90763d0bc202053c5a3370d2b532
halimov <halimov@infomatika.ru>
2026-07-31 16:58:47 +0300

    add logo

---
+++ app/components/Logo.tsx
@@ -0,0 +1,7 @@
+import "@styles/logo.css";
+
+export default function Logo() {
+    return (
+        <img src="/logo.png" alt="NovaMarket" className="logo" />
+    )
+}
--- app/layouts/auth.tsx
+++ app/layouts/auth.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "../+types/root";
 import { Outlet } from "react-router";
 import '@styles/auth-layout.css';
+import Logo from "~/components/Logo";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,6 +13,7 @@ export function meta({}: Route.MetaArgs) {
 export default function AuthLayout() {
     return (
         <div className="layoyt auth-layout">
+          <Logo />
           <Outlet />
         </div>
     )
