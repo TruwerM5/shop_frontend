bd3a12aa8bd441b6e27555d3340130f3b215a3c8
halimov <halimov@infomatika.ru>
2026-08-04 17:39:38 +0300

    add product types

---
+++ types/product.ts
@@ -0,0 +1,28 @@
+
+export interface CatalogStore {
+    products: ApiGetProductItem[];
+    fetchCatalog: () => Promise<ApiGetProductItem[]>;
+    getCatalog: () => ApiGetProductItem[];
+}
+
+export interface ApiGetProductItem {
+    productId: number;
+    name: string;
+    price: number;
+    sellerId: number;
+    productImages: ApiGetProductImages[];
+}
+
+export interface ApiGetProductImages {
+    productId: number;
+    imagePath: string;
+}
+
+export interface ApiGetProductDetails {
+    productId: number;
+    category: string;
+    size: string | null;
+    color: string | null;
+    author?: string;
+    description?: string;
+}
