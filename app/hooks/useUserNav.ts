525a8e21a5615e2c228dfc9bf7b6d933e6896455
halimov <halimov@infomatika.ru>
2026-08-04 14:11:48 +0300

    change useUserNav

--- app/hooks/useUserNav.ts
+++ app/hooks/useUserNav.ts
@@ -1,37 +1,7 @@
 import type { NavItem } from '../../types/nav';
 import { baseHeaderLinks } from '~/common/nav-links';
-import { useUserStore } from '~/stores/user.store';
-import { UserRole } from '../../types/user';
-
 
 export function useUserNav(): NavItem[] {
-    const user = useUserStore((state) => state.user);
-
-    if(!user) {
-        return baseHeaderLinks;
-    }
-
-    if(user.role === UserRole.user) {
-        return getUserNav();
-    }
-
-    return [];
+    return baseHeaderLinks;
 } 
 
-function getUserNav(): NavItem[] {
-    return [
-        ...baseHeaderLinks,
-        {
-            id: 3,
-            title: 'Orders',
-            href: '/orders',
-            type: 'link',
-        },
-        {
-            id: 4,
-            title: 'Profile',
-            href: '/profile',
-            type: 'link',
-        },
-    ]
-}
