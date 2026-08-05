a83c22e1c7a3ab2e25ae52255d3ed9cc4dbec0fb
halimov <halimov@infomatika.ru>
2026-08-05 23:24:10 +0300

    init auth middleware for orders

--- app/routes/orders.tsx
+++ app/routes/orders.tsx
@@ -1,4 +1,13 @@
-export default function OrdersPage() {
+import { useUserStore } from "~/stores/user.store"
+import { Navigate } from "react-router";
+
+export default function OrdersPage() {        
+    const isAuthenticated = useUserStore((state) => state.isAuthenticated);
+
+    if(!isAuthenticated) {
+       return <Navigate to="/login" replace />
+    }
+
     return (
         <div className="page orders-page">
             <h1>Orders</h1>
