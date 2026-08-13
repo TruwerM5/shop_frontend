f38750831eed33691428a8040cc5095b0e6bf5d6
halimov <halimov@infomatika.ru>
2026-08-13 15:52:54 +0300

    replace description down

---
+++ app/components/Description/Description.tsx
@@ -0,0 +1,41 @@
+import "./description.css";
+import { useState } from "react";
+import clsx from "clsx";
+export default function ShortDesciption({
+    link,
+    description,
+}: {
+    link: `#${string}`;
+    description?: string
+}) {
+    if(!description) {
+        return null;
+    }
+
+    const [isOpened, setIsOpened] = useState(false);
+
+    function handleClick() {
+        setIsOpened(!isOpened);
+    }
+
+    const descriptionInnerClass = clsx("description__inner", {
+        "description__inner_opened": isOpened,
+    });
+
+    return (
+        <div className="description">
+            <h5 className="title-sm">Description</h5>
+            <div className={descriptionInnerClass}>
+                <p className="description__text">
+                    {description}
+                </p>
+            </div>
+            <button 
+                onClick={handleClick} 
+                className="description__toggle-button"
+            >
+                {isOpened ? 'Hide' : 'Show more'}
+            </button>
+        </div>
+    )
+}
